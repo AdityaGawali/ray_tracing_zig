ef61b7d412bbf390d38338ce3fe5b344eaad3607
Aditya Gawali <adityagawali1511@gmail.com>
2025-09-19 23:27:37 -0700

    write pixels to ppm file

---
+++ README.md
@@ -0,0 +1,11 @@
+## Ray tracing  in zig
+
+[Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
+- To build and run
+```zig build run``` --> Generates `out.ppm`
+- If ppm viewer is not available 
+- download and install `imagemagick` 
+  ```sudo apt install imagemagick ```
+- Convert .ppm into png 
+   ``` convert out.ppm out.png```
+
---
+++ src/main.zig
@@ -0,0 +1,39 @@
+const std = @import("std");
+
+const image_width: u32 = 1024;
+const image_height: u32 = 960;
+var tmp: [128]u8 = undefined;
+
+pub fn main() !void {
+    var file = try std.fs.cwd().createFile("out.ppm", .{ .truncate = true });
+    defer file.close();
+
+    var line = try std.fmt.bufPrint(&tmp, "{s}\n", .{"P3"});
+    _ = try file.write(line);
+
+    line = try std.fmt.bufPrint(&tmp, "{d} {d}\n{d}\n", .{ image_width, image_height, 255 });
+    _ = try file.write(line);
+
+    const img_ht_f: f64 = @as(f64, @floatFromInt((image_height)));
+    const img_wd_f: f64 = @as(f64, @floatFromInt((image_width)));
+
+    for (0..image_height) |x| {
+        for (0..image_width) |y| {
+            const x_f: f64 = @as(f64, @floatFromInt(x));
+            const y_f: f64 = @as(f64, @floatFromInt(y));
+
+            const red = x_f / (img_wd_f - 1.0);
+            const green = y_f / (img_ht_f - 1.0);
+            const blue = 0.0;
+
+            const r_px: usize = @intFromFloat(@floor(red * 255.99));
+            const g_px: usize = @intFromFloat(@floor(green * 255.99));
+            const b_px: usize = @intFromFloat(@floor(blue * 255.99));
+
+            line = try std.fmt.bufPrint(&tmp, "{d} {d} {d}\n", .{ r_px, g_px, b_px });
+            _ = try file.write(line);
+        }
+    }
+
+    try file.sync();
+}
